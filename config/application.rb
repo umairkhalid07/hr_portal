759a48a2cbed33c9e5024648c755c3d323bccfea
umair-khalid-brainx <umair.khalid@brainxtech.com>
2022-11-22 11:26:48 +0500

    Active Storage

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,6 @@ module HrPortal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
-      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
-      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
-    }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
